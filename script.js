a9ca06d9d5b10290d68579d088bb67caa21018ed
kxGeou <grzegorzm.work@gmail.com>
2024-07-18 21:34:15 +0200

    the end

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ let secondCur = document.querySelector(".secondCurrency");
 let result = document.querySelector(".currencyText");
 let buttons = document.querySelectorAll(".currency");
 const exchangeRateEuro = 4.29;
+const exchangeRateUSD = 3.94;
+const exchangeRateGBP = 5.10;
+const exchangeRateCZK = 0.17;
 
 const swapEuro = () => {
   if (secondCur.innerHTML !== "EUR") {
     secondCur.innerHTML = "EUR";
+    result.innerHTML = '';
     resetStyleButton();
     buttons[0].style.background = "white";
     buttons[0].style.color = "black";
@@ -20,6 +24,7 @@ const swapEuro = () => {
 const swapUsd = () => {
   if (secondCur.innerHTML !== "USD") {
     secondCur.innerHTML = "USD";
+    result.innerHTML = '';
     resetStyleButton();
     buttons[1].style.background = "white";
     buttons[1].style.color = "black";
@@ -31,6 +36,7 @@ const swapUsd = () => {
 const swapGbp = () => {
   if (secondCur.innerHTML !== "GBP") {
     secondCur.innerHTML = "GBP";
+    result.innerHTML = '';
     resetStyleButton();
     buttons[2].style.background = "white";
     buttons[2].style.color = "black";
@@ -42,6 +48,7 @@ const swapGbp = () => {
 const swapCzk = () => {
   if (secondCur.innerHTML !== "CZK") {
     secondCur.innerHTML = "CZK";
+    result.innerHTML = '';
     resetStyleButton();
     buttons[3].style.background = "white";
     buttons[3].style.color = "black";
@@ -54,22 +61,35 @@ const exchanger = () => {
   if (converter.value === "") {
     result.innerHTML = "Musisz wpisać ilość zł!";
     converter.classList.add("noDataInput");
+
   } else if (converter.value === '0') {
     result.innerHTML = "Liczba musi być różna od zera!";
+
   } else if (secondCur.innerHTML === "???") {
     converter.classList.remove("noDataInput");
     result.innerHTML = "Musisz wybrać walutę do przeliczenia!";
     buttons.forEach((button) => {
       button.classList.add("noDataInput");
     });
+
   } else {
     converter.classList.remove("noDataInput");
     buttons.forEach((button) => {
       button.classList.remove("noDataInput");
     });
+
     if (secondCur.innerHTML === "EUR") {
       let eurAmount = (converter.value / exchangeRateEuro).toFixed(2);
-      result.innerHTML = `${converter.value} ZŁ to ${eurAmount} EURO`;
+      result.innerHTML = `${converter.value} ZŁ to ${eurAmount} €`;
+    } else if (secondCur.innerHTML === "USD") {
+        let usdAmount = (converter.value / exchangeRateUSD).toFixed(2);
+        result.innerHTML = `${converter.value} ZŁ to ${usdAmount} $ `;
+    } else if (secondCur.innerHTML === "GBP") {
+        let gbpAmount = (converter.value / exchangeRateGBP).toFixed(2);
+        result.innerHTML = `${converter.value} ZŁ to ${gbpAmount} £`;
+    } else if (secondCur.innerHTML === 'CZK') {
+        let czkAmount = (converter.value / exchangeRateCZK).toFixed(2);
+        result.innerHTML = `${converter.value} ZŁ to ${czkAmount} Kč`;
     }
   }
 };
@@ -88,11 +108,3 @@ buttons[2].addEventListener("click", swapGbp);
 buttons[3].addEventListener("click", swapCzk);
 buttonExchanger.addEventListener("click", exchanger);
 
-
-/* ZAŁOŻENIA
-    - Użytkownik po kliknieciu przelicz dostaje komunikat na boxie 2 o tym ile zl to euro,funt itp
-    - Po przeliczeniu input automatycznie się czyści 
-    - Tekst na środku (wykrywacz walut) zmienia sie po kliknieciu na daną walutę
-    - Po kliknieciu na dana walute przycisk zaczyna sie podswietlac
-    - Przy przęłączaniu walut input się nie czyści
-*/
